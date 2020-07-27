11c55763aae3f12ac8d74d5f86cd953c75672a70
Elizabeth <elizabeth.graham2000@gmail.com>
2020-07-26 20:07:36 -0500

    Fixed left and right turn functions
    Added more comments too

--- MotorTestingScript.py
+++ MotorTestingScript.py
@@ -1,11 +1,73 @@
 from adafruit_motorkit import MotorKit
 from  pynput import keyboard
+
 kit = MotorKit()
 
+# Park motors
 kit.motor1.throttle = 0.0
 kit.motor2.throttle = 0.0
 kit.motor3.throttle = 0.0
 kit.motor4.throttle = 0.0
+
+# Motor configuration dependant on individual build.
+# Motor 1 = front left
+# Motor 2 = back left
+# Motor 3 = front right
+# Motor 4 = back right
+
+# Motor 1 + 3 are positive facing forward, 2 + 4 are negative facing forward
+def motorsForward():
+    kit.motor1.throttle = 1.0
+    kit.motor2.throttle = -1.0
+    kit.motor3.throttle = 1.0
+    kit.motor4.throttle = -1.0
+    
+def motorsBackward():
+    kit.motor1.throttle = -1.0
+    kit.motor2.throttle = 1.0
+    kit.motor3.throttle = -1.0
+    kit.motor4.throttle = 1.0
+    
+# Right motors (3,4) will arc around left motors (1,2) to turn left
+def motorsLeft():
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+    kit.motor3.throttle = 1.0
+    kit.motor4.throttle = -1.0
+
+# Left motors (1,2) will arc around right motors (3,4) to turn right
+def motorsRight():
+    kit.motor1.throttle = 1.0
+    kit.motor2.throttle = -1.0
+    kit.motor3.throttle = 0.0
+    kit.motor4.throttle = 0.0
+
+def motorsStop():
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+    kit.motor3.throttle = 0.0
+    kit.motor4.throttle = 0.0
+    
+def motorOneTest():
+    kit.motor1.throttle = 1.0
+    kit.motor2.throttle = 0.0
+    kit.motor3.throttle = 0.0
+    kit.motor4.throttle = 0.0
+def motorTwoTest():
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 1.0
+    kit.motor3.throttle = 0.0
+    kit.motor4.throttle = 0.0
+def motorThreeTest():
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+    kit.motor3.throttle = 1.0
+    kit.motor4.throttle = 0.0
+def motorFourTest():
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+    kit.motor3.throttle = 0.0
+    kit.motor4.throttle = 1.0
 
 def on_press(key):
     try:
@@ -47,57 +109,6 @@ def on_release(key):
     else:
         motorsStop()
         print("Motors Released")
-    
-def motorsForward():
-    kit.motor1.throttle = 1.0
-    kit.motor2.throttle = -1.0
-    kit.motor3.throttle = 1.0
-    kit.motor4.throttle = -1.0
-    
-def motorsBackward():
-    kit.motor1.throttle = -1.0
-    kit.motor2.throttle = 1.0
-    kit.motor3.throttle = -1.0
-    kit.motor4.throttle = 1.0
-    
-def motorsLeft():
-    kit.motor1.throttle = 1.0
-    kit.motor2.throttle = 1.0
-    kit.motor3.throttle = 0.0
-    kit.motor4.throttle = 0.0
-    
-def motorsRight():
-    kit.motor1.throttle = 0.0
-    kit.motor2.throttle = 0.0
-    kit.motor3.throttle = 1.0
-    kit.motor4.throttle = 1.0
-
-def motorsStop():
-    kit.motor1.throttle = 0.0
-    kit.motor2.throttle = 0.0
-    kit.motor3.throttle = 0.0
-    kit.motor4.throttle = 0.0
-    
-def motorOneTest():
-    kit.motor1.throttle = 1.0
-    kit.motor2.throttle = 0.0
-    kit.motor3.throttle = 0.0
-    kit.motor4.throttle = 0.0
-def motorTwoTest():
-    kit.motor1.throttle = 0.0
-    kit.motor2.throttle = 1.0
-    kit.motor3.throttle = 0.0
-    kit.motor4.throttle = 0.0
-def motorThreeTest():
-    kit.motor1.throttle = 0.0
-    kit.motor2.throttle = 0.0
-    kit.motor3.throttle = 1.0
-    kit.motor4.throttle = 0.0
-def motorFourTest():
-    kit.motor1.throttle = 0.0
-    kit.motor2.throttle = 0.0
-    kit.motor3.throttle = 0.0
-    kit.motor4.throttle = 1.0
 
 # ...or, in a non-blocking fashion:
 listener = keyboard.Listener(
