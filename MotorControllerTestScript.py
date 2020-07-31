5e5e6f26e2a64b026a150f5097cc6539e5007885
ElizabethGraham <59736864+ElizabethGraham@users.noreply.github.com>
2020-07-30 19:45:34 -0500

    Consolidated test scripts and deleted now unneeded files. Converted if statements in MotorControllerTestScript.py to a if statement referring to a dictionary with the possible key presses. Added TextToSpeech.py. Now using GTTS for text to speech functions and Pygame as an mp3 player. Would like to exchange Pygame for a more lightweight and less clunky mp3 player library (GTTS too, preferably) but Pygame was the most agreeable as a proof of concept.

--- MotorControllerTestScript.py
+++ MotorControllerTestScript.py
@@ -1,5 +1,5 @@
-from adafruit_motorkit import MotorKit
-from  pynput import keyboard
+from adafruit_motorkit import MotorKit # Error on Windows, functions on Raspberry Pi
+from pynput import keyboard
 
 kit = MotorKit()
 
@@ -9,6 +9,7 @@ kit.motor2.throttle = 0.0
 kit.motor3.throttle = 0.0
 kit.motor4.throttle = 0.0
 
+
 # Motor configuration dependant on individual build.
 # Motor 1 = front left
 # Motor 2 = back left
@@ -16,99 +17,94 @@ kit.motor4.throttle = 0.0
 # Motor 4 = back right
 
 # Motor 1 + 3 are positive facing forward, 2 + 4 are negative facing forward
-def motorsForward():
+def motors_forward():
     kit.motor1.throttle = 1.0
     kit.motor2.throttle = -1.0
     kit.motor3.throttle = 1.0
     kit.motor4.throttle = -1.0
-    
-def motorsBackward():
+
+
+def motors_backward():
     kit.motor1.throttle = -1.0
     kit.motor2.throttle = 1.0
     kit.motor3.throttle = -1.0
     kit.motor4.throttle = 1.0
-    
+
+
 # Right motors (3,4) will arc around left motors (1,2) to turn left
-def motorsLeft():
+def motors_left():
     kit.motor1.throttle = 0.0
     kit.motor2.throttle = 0.0
     kit.motor3.throttle = 1.0
     kit.motor4.throttle = -1.0
 
+
 # Left motors (1,2) will arc around right motors (3,4) to turn right
-def motorsRight():
+def motors_right():
     kit.motor1.throttle = 1.0
     kit.motor2.throttle = -1.0
     kit.motor3.throttle = 0.0
     kit.motor4.throttle = 0.0
 
-def motorsStop():
+
+def motors_stop():
     kit.motor1.throttle = 0.0
     kit.motor2.throttle = 0.0
     kit.motor3.throttle = 0.0
     kit.motor4.throttle = 0.0
-    
-def motorOneTest():
+
+
+def motor_one_test():
     kit.motor1.throttle = 1.0
     kit.motor2.throttle = 0.0
     kit.motor3.throttle = 0.0
     kit.motor4.throttle = 0.0
-def motorTwoTest():
+
+
+def motor_two_test():
     kit.motor1.throttle = 0.0
     kit.motor2.throttle = 1.0
     kit.motor3.throttle = 0.0
     kit.motor4.throttle = 0.0
-def motorThreeTest():
+
+
+def motor_three_test():
     kit.motor1.throttle = 0.0
     kit.motor2.throttle = 0.0
     kit.motor3.throttle = 1.0
     kit.motor4.throttle = 0.0
-def motorFourTest():
+
+
+def motor_four_test():
     kit.motor1.throttle = 0.0
     kit.motor2.throttle = 0.0
     kit.motor3.throttle = 0.0
     kit.motor4.throttle = 1.0
 
+
+# Dictionary holding all movement keys and corresponding movement functions
+keypress_dict = {"w": motors_forward, "a": motors_left, "s": motors_backward,
+                 "d": motors_right, "1": motor_one_test, "2": motor_two_test,
+                 "3": motor_three_test, "4": motor_four_test, "space": motors_stop}
+
+
 def on_press(key):
     try:
-        if key.char == "w":
-            motorsForward()
-            print("Forward")
-        elif key.char == "a":
-            motorsLeft()
-            print("Left")
-        elif key.char == "s":
-            motorsBackward()
-            print("Reverse")
-        elif key.char == "space":
-            motorsStop()
-            print("Stop")
-        elif key.char == "d":
-            motorsRight()
-            print("Right")
-        elif key.char == "1":
-            motorOneTest()
-            prindaadaadadaaaat("1")
-        elif key.char == "2":
-            motorTwoTest()
-            print("2")
-        elif key.char == "3":
-            motorThreeTest()
-            print("3")
-        elif key.char == "4":
-            motorFourTest()
-            print("4")
+        if key.char in keypress_dict:
+            keypress_dict[key.char]()
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
+
 
 def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
     else:
-        motorsStop()
+        motors_stop()
         print("Motors Released")
+
 
 # ...or, in a non-blocking fashion:
 listener = keyboard.Listener(
--- Plato_Alpha_v0.1.py
+++ Plato_Alpha_v0.1.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+
 pygame.init()
 
 # assigning values to X and Y variable 
@@ -8,13 +9,15 @@ Y = 370
 
 # define the RGB value 
 # for white colour 
-white = (255, 255, 255) 
+white = (255, 255, 255)
 
 # Set up the drawing window
 screen = pygame.display.set_mode([X, Y])
 pygame.display.set_caption("Plato Alpha")
 
-characterEmotions = [pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_HAPPY.png'), pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_SAD.png'), pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_WINK.png')]
+characterEmotions = [pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_HAPPY.png'),
+                     pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_SAD.png'),
+                     pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_WINK.png')]
 
 isHappy = True
 isWinking = False
@@ -22,30 +25,34 @@ isSad = False
 
 # create the display surface object 
 # of specific dimension..e(X, Y). 
-display_surface = pygame.display.set_mode((X, Y )) 
+display_surface = pygame.display.set_mode((X, Y))
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill(white)
 
-def emotionHappy():
-    print("HAPPY") # Print to console the function is runnning, for debug purposes
-    emotion = 0 # Happy image in characterEmotions image array
-    display_surface.blit(background, (0,0)) # Reset canvas
-    display_surface.blit(characterEmotions[emotion], (0, 0)) # Print the happy emotion picture
+
+def emotion_happy():
+    print("HAPPY")  # Print to console the function is runnning, for debug purposes
+    emotion = 0  # Happy image in characterEmotions image array
+    display_surface.blit(background, (0, 0))  # Reset canvas
+    display_surface.blit(characterEmotions[emotion], (0, 0))  # Print the happy emotion picture
     # Play characterEmotionSound>Happy
-    
-def emotionSad():
+
+
+def emotion_sad():
     print("SAD")
     emotion = 1
-    display_surface.blit(background, (0,0))
+    display_surface.blit(background, (0, 0))
     display_surface.blit(characterEmotions[emotion], (0, 0))
 
-def actionWink():
+
+def action_wink():
     print("WINK")
     emotion = 2
-    display_surface.blit(background, (0,0))
+    display_surface.blit(background, (0, 0))
     display_surface.blit(characterEmotions[emotion], (0, 0))
+
 
 # Run until the user asks to quit
 running = True
@@ -55,22 +62,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.display.update()
-        
+
     emotion = 0
-    
-    
+
     keys = pygame.key.get_pressed()
-    
+
     if keys[pygame.K_SPACE]:
-        emotionHappy()        
+        emotion_happy()
     elif keys[pygame.K_RIGHT]:
-        actionWink()
+        action_wink()
     elif keys[pygame.K_DOWN]:
-        emotionSad()
-    
+        emotion_sad()
+
     # Flip the display
     pygame.time.delay(100)
     pygame.display.update()
-     
+
 # Done! Time to quit.
 pygame.quit()
