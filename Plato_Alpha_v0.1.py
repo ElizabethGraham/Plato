47d50034d68db4ead3c2c22b80c172ff7224021e
GoodbyeBlues <elizabeth.graham2000@gmail.com>
2021-02-26 10:47:16 -0600

    Removed emotion index int variable It's really unneeded

--- Plato_Alpha_v0.1.py
+++ Plato_Alpha_v0.1.py
@@ -15,9 +15,9 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode([X, Y])
 pygame.display.set_caption("Plato Alpha")
 
-characterEmotions = [pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_HAPPY.png'),
-                     pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_SAD.png'),
-                     pygame.image.load(r'/home/pi/Documents/Plato/Plato-master/plato_pics/ROBOT_WINK.png')]
+characterEmotions = [pygame.image.load(r'C:/Users/eliza/OneDrive/Documents/GitHub/Plato/plato_pics/ROBOT_HAPPY.png'), # C:\Users\eliza\OneDrive\Documents\GitHub\Plato\plato_pics\ROBOT_SAD.png
+                     pygame.image.load(r'C:/Users/eliza/OneDrive/Documents/GitHub/Plato/plato_pics/ROBOT_SAD.png'),
+                     pygame.image.load(r'C:/Users/eliza/OneDrive/Documents/GitHub/Plato/plato_pics/ROBOT_WINK.png')]
 
 isHappy = True
 isWinking = False
@@ -34,24 +34,21 @@ background.fill(white)
 
 def emotion_happy():
     print("HAPPY")  # Print to console the function is runnning, for debug purposes
-    emotion = 0  # Happy image in characterEmotions image array
     display_surface.blit(background, (0, 0))  # Reset canvas
-    display_surface.blit(characterEmotions[emotion], (0, 0))  # Print the happy emotion picture
+    display_surface.blit(characterEmotions[0], (0, 0))  # Print the happy emotion picture
     # Play characterEmotionSound>Happy
 
 
 def emotion_sad():
     print("SAD")
-    emotion = 1
     display_surface.blit(background, (0, 0))
-    display_surface.blit(characterEmotions[emotion], (0, 0))
+    display_surface.blit(characterEmotions[1], (0, 0))
 
 
 def action_wink():
     print("WINK")
-    emotion = 2
     display_surface.blit(background, (0, 0))
-    display_surface.blit(characterEmotions[emotion], (0, 0))
+    display_surface.blit(characterEmotions[2], (0, 0))
 
 
 # Run until the user asks to quit
@@ -63,11 +60,9 @@ while running:
             running = False
             pygame.display.update()
 
-    emotion = 0
-
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_UP]:
         emotion_happy()
     elif keys[pygame.K_RIGHT]:
         action_wink()
